7dde50d34663f5a7b287df8e015a6f8ead6cd43d
vastille <vdolcine@gmail.com>
2026-05-25 11:38:46 -0700

    finished part2c: getting data from server

--- part2/app/src/App.jsx
+++ part2/app/src/App.jsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import Note from "./components/Note"
 
-const App = (props) => {
-	const [notes, setNotes] = useState(props.notes)
+const App = () => {
+	const [notes, setNotes] = useState([])
 	const [newNote, setNewNote] = useState('')
 	const [showAll, setShowAll] = useState(true)
+
+	useEffect(() => {
+		console.log('effect')
+		axios
+		.get('http://localhost:3001/notes')
+		.then(response => {
+			console.log('promise fulfilled')
+			setNotes(response.data)
+		})
+	}, [])
+	console.log('render', notes.length, 'notes')
 
 	const handleAddNote = (event) => {
 		event.preventDefault()
@@ -19,7 +31,6 @@ const App = (props) => {
 	}
 
 	const handleNoteChange = (event) => {
-		console.log(event.target.value)
 		setNewNote(event.target.value)
 	}
 
